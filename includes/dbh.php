b6da915adae6d3d268469d615a7de27a78d269c7
alcmene00 <45488168+alcmene00@users.noreply.github.com>
2021-06-14 21:45:23 +0300

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -5,3 +5,19 @@ $password = "";
 $dbname = "day_zero_db";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
+
+if (isset($_POST["submit"])){
+    $title = "image";
+    $pname = rand(1000,10000)."-".$_FILES["file"]["name"];
+    $tname = $_FILES["file"]["tmp_name"];
+    $uploads = 'pictures/profile_pictures';
+    move_uploaded_file($tname, $uploads.'/'.$pname);
+    $sql = "INSERT into fileup(title,image) VALUES('$title','$pname')";
+    if(mysqli_query($conn,$sql)){
+        echo "File Sucessfully uploaded";
+    }
+    else{
+        echo "Error";
+    }
+}
+?>
